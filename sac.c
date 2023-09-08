fa0e19da3c62752040200781e9f035710b6aa2ad
Lucca Cruz Pasquali <luccapasquali@hotmail.com>
2023-09-08 13:59:48 -0300

    Inicio programa

--- sac.c
+++ sac.c
@@ -1,3 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int main (){
+    
+    int respUsu;
+    
+    printf("Bem vindo ao Servico de Atentdimento ao Cliente ATIM!\n");
+    printf("Para iniciar seu atendimento digite uma um número referente ao motivo do atendimento: \n");
+    printf("\nDigite [1] para solicitar suporte tecnico\n");
+    printf("\nDigite [2] para cancelamento da linha\n");
+    printf("\nDigite [3] para problemas com a fatura\n");
+    printf("\nDigite [4] para encerrar\n");
+    scanf("%d",&respUsu);
+    
+    if (respUsu == 1){
+    	
+    	printf("");
+    	
+	} else if (respUsu == 2){
+		
+	} else if (respUsu == 3) {
+		
+	} else {
+		return 1;
+	}
+    
+    
+    
+    
     
 }
